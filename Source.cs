956b01f0ad96ec627a5a56affd4bcea316962bbf
sirkz <golang.freelance@gmail.com>
2021-04-05 23:58:36 +0700

    Rework

--- Source.cs
+++ Source.cs
@@ -23,11 +23,11 @@ namespace AlgorithmsDataStructures
         public void Put(T value)
         {
             int slot = HashFun(value);
-            if (slots[slot] == null)
+            if (!Get(value))
             {
                 size++;
+                slots[slot].AddLast(value);
             }
-            slots[slot] = value;
         }
 
         public bool Get(T value)
@@ -43,7 +43,7 @@ namespace AlgorithmsDataStructures
         {
             if (Get(value))
             {
-                slots[HashFun(value)] = default;
+                slots[HashFun(value)].Remove(value);
                 size--;
                 return true;
             }
@@ -53,11 +53,14 @@ namespace AlgorithmsDataStructures
         public PowerSet<T> Intersection(PowerSet<T> set2)
         {
             PowerSet<T> result = new PowerSet<T>();
-            foreach (T item in slots)
+            foreach (var slot in slots)
             {
-                if (item != null && set2.Get(item))
+                foreach (var item in slot)
                 {
-                    result.Put(item);
+                    if (set2.Get(item))
+                    {
+                        result.Put(item);
+                    }
                 }
             }
             return result;
@@ -66,16 +69,16 @@ namespace AlgorithmsDataStructures
         public PowerSet<T> Union(PowerSet<T> set2)
         {
             PowerSet<T> result = new PowerSet<T>();
-            foreach (T item in slots)
+            foreach (var slot in slots)
             {
-                if (item != null)
+                foreach (var item in slot)
                 {
                     result.Put(item);
                 }
             }
-            foreach (T item in set2.slots)
+            foreach (var slot in set2.slots)
             {
-                if (item != null)
+                foreach (var item in slot)
                 {
                     result.Put(item);
                 }
@@ -86,16 +89,16 @@ namespace AlgorithmsDataStructures
         public PowerSet<T> Difference(PowerSet<T> set2)
         {
             PowerSet<T> result = new PowerSet<T>();
-            foreach (T item in slots)
+            foreach (var slot in slots)
             {
-                if (item != null)
+                foreach (var item in slot)
                 {
                     result.Put(item);
                 }
             }
-            foreach (T item in set2.slots)
+            foreach (var slot in set2.slots)
             {
-                if (item != null)
+                foreach (var item in slot)
                 {
                     result.Remove(item);
                 }
@@ -105,38 +108,58 @@ namespace AlgorithmsDataStructures
 
         public bool IsSubset(PowerSet<T> set2)
         {
-            foreach (T item in set2.slots)
+            foreach (var slot in set2.slots)
             {
-                if (item != null && !Get(item))
+                foreach (var item in slot)
                 {
-                    return false;
+                    if (!Get(item))
+                    {
+                        return false;
+                    }
                 }
             }
             return true;
+        }
+
+        public override bool Equals(object _set2)
+        {
+            var set2 = _set2 as PowerSet<T>;
+
+            if (set2 == null) return false;
+
+            if (Size() != set2.Size()) return false;
+            if (IsSubset(set2) && set2.IsSubset(this)) return true;
+            return false;
         }
     }
 
     public class HashTable<T>
     {
         public int sze;
-        public T[] slots;
+        public LinkedList<T>[] slots;
 
         public HashTable()
         {
             sze = 20000;
-            slots = new T[sze];
-            for (int i = 0; i < sze; i++) slots[i] = default;
+            slots = new LinkedList<T>[sze];
+            for (int i = 0; i < sze; i++) slots[i] = new LinkedList<T>();
         }
 
         public int HashFun(T value)
         {
+            if (typeof(T) == typeof(string))
+            {
+                int sum = 0;
+                Array.ForEach((value as string).ToCharArray(), delegate (char i) { sum += i; });
+                return sum % sze;
+            }
             return (value.GetHashCode() & 0x7FFFFFFF) % sze;
         }
 
         public int Find(T value)
         {
             int slot = HashFun(value);
-            if (slots[slot] != null && slots[slot].Equals(value))
+            if (slots[slot].Find(value) != null)
             {
                 return slot;
             }
